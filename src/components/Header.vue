b3ec4b362744a34788d76f894899e4f0e5cee39f
Arvid Strauss <avst@mms-t-systems.de>
2020-01-21 16:05:35 +0100

    fixed header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,8 +35,8 @@ export default {
 }
 
 .top-nav-bar .search-box img.logo {
-  margin: 10px 50px;
-  width: 80px;
+  margin: 10px 20px;
+  width: 70px;
 }
 
 .top-nav-bar .search-box a {
@@ -51,51 +51,9 @@ export default {
   font-size: 38px;
 }
 
-.menu-bar {
-  width: 40%;
-  height: 57px;
-  float: right;
-}
-
-.menu-bar ul {
-  display: inline-flex;
-  float: right;
-}
-
-.menu-bar ul li {
-  border-left: 1px solid var(--white);
-  list-style-type: none;
-  padding: 15px 35px;
-  text-align: center;
-  background-color: var(--cyan);
-  cursor: pointer;
-}
-
-.menu-bar ul li:hover {
-  background-color: var(--darkgrey);
-}
-
-.menu-bar ul li a {
-  font: bold 16px;
-  text-decoration: none;
-  color: var(--white);
-}
-
-.menu-bar ul li a .fa-home {
-  margin-right: 5px;
-}
-
-.menu-bar ul li a .fa-user-plus {
-  margin-right: 5px;
-}
-
-.menu-bar ul li a .fa-user {
-  margin-right: 5px;
-}
-
 @media only screen and (max-width: 1030px) {
   .top-nav-bar .search-box img.logo {
-    width: 40px;
+    width: 80px;
   }
   .top-nav-bar {
     height: 118px;
@@ -106,18 +64,6 @@ export default {
   }
   .top-nav-bar .search-box h1 {
     font-size: 32px;
-  }
-  .menu-bar {
-    width: 100%;
-  }
-  .menu-bar ul {
-    margin: 10px 0;
-    width: 100%;
-  }
-  .menu-bar ul li {
-    font-size: 18px;
-    height: 70px;
-    width: 100%;
   }
 }
 </style>
